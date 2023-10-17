ac4242482d0d33f1c91d4cce87f263f8e2f60542
Romulo Brandão <42874556+itsRomulo@users.noreply.github.com>
2023-10-17 20:43:24 -0300

    Consulta de atendimentos

--- processes/atendimento.py
+++ processes/atendimento.py
@@ -1,9 +1,23 @@
 from flask import Flask, redirect, render_template
+from libs.postgresTool import execute_query_df
+from libs.utils import *
+import json
 
 def rotas_atendimento(app):
 
-    @app.route('/atendimento', methods=['POST'])
-    def atendimento():
-        return {'status':'ok'}
-    
+    @app.route('/atendimento-consulta', methods=['POST'])
+    def atendimento_consulta():
+        try:
+            return {'status':'ok', 'data':get_df_to_json()}
+        except Exception as e:
+            return {'status':'error', 'data':str(e)}
     return app
+
+
+def get_df_to_json():
+    query = """SELECT id_atendimento, nome, cpf, especialidade, profissional_responsavel, observacao, data_inicio, data_fim, id_paciente, id_profissional
+	FROM public.atendimento;"""
+    df = execute_query_df(query)
+    js = df_to_json(df)
+    js = json.loads(js)
+    return js
