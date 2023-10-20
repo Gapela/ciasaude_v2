0b98cd12823657bd8a92eaaa0090c4ecabea1f26
Romulo Brandão <42874556+itsRomulo@users.noreply.github.com>
2023-10-20 00:14:39 -0300

    login finalizado + navbar

--- frontend/login/static/js/login.js
+++ frontend/login/static/js/login.js
@@ -1,1 +1,10 @@
-alert("teste");
+//função para pegar os dados do formulário e enviar para o servidor
+function login() {
+  //pegar os dados do formulário
+  var dados = {
+    usuario: document.getElementById("user").value,
+    senha: document.getElementById("password").value,
+  };
+  //redirecionar para a homepage
+  window.location.href = "/home";
+}
--- frontend/page/page.py
+++ frontend/page/page.py
@@ -1,4 +1,10 @@
 from flask import Blueprint, render_template, redirect
 
 
-page_bp = Blueprint("page", __name__, template_folder="templates", static_folder='static', static_url_path='/page/static')
+page_bp = Blueprint("page", __name__, template_folder="templates",
+                    static_folder='static', static_url_path='/page/static')
+
+
+@page_bp.route("/home")
+def homepage():
+    return render_template("homepage.html")
