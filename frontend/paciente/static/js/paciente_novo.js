8e6e8762c3c96fc69d43c886815bd2e8b7593703
Romulo Brandão <42874556+itsRomulo@users.noreply.github.com>
2023-10-21 17:14:29 -0300

    Terminando o cadastro de paciente

---
+++ frontend/paciente/static/js/paciente_novo.js
@@ -0,0 +1,22 @@
+function get_content() {
+  const inputs = document.querySelectorAll("input");
+
+  json = {};
+  for (let i = 0; i < inputs.length; i++) {
+    //converter os inputs em json usando o id do input como chave e a value como valor
+    json[inputs[i].id] = inputs[i].value;
+
+    //verifica se o input é do tipo checkbox
+    if (inputs[i].type == "checkbox") {
+      //se for, desmarca o checkbox
+      inputs[i].checked = false;
+    } else {
+      //se não for, limpa o campo
+      inputs[i].value = "";
+    }
+  }
+
+  //pegar os valores do textarea e do select
+  json["descricao"] = document.querySelector("textarea").value;
+  json["tipo"] = document.querySelector("select").value;
+}
