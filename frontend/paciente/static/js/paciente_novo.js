f7739bbe6d6dabac13836c6386f674fb35baac63
A5_gpelai <gpelai@a5solutions.com>
2023-12-08 01:59:34 -0300

    criação de algumas funções em js para a validação dos campos no forma de novo_paciente. Ainfa não concluódo

--- frontend/paciente/static/js/paciente_novo.js
+++ frontend/paciente/static/js/paciente_novo.js
@@ -15,3 +15,107 @@ function get_content() {
   console.log(res);
   return form;
 }
+
+function formatarCEP(cep) {
+  var cepFormatado = cep.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+  cepFormatado = cepFormatado.replace(/^(\d{5})(\d)/, '$1-$2'); // Insere um hífen após o quinto dígito
+  if (cepFormatado.length > 9) {
+      cepFormatado = cepFormatado.substring(0, 9); // Limita o comprimento a 9 caracteres
+  }
+  return cepFormatado;
+}
+
+function formatarRG(rg) {
+  var rgFormatado = rg.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+  rgFormatado = rgFormatado.replace(/^(\d{2})(\d)/, '$1.$2'); // Insere um ponto após o segundo dígito
+  rgFormatado = rgFormatado.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3'); // Insere um ponto após o quinto dígito
+  rgFormatado = rgFormatado.replace(/^(\d{2})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3-$4'); // Insere um hífen após o oitavo dígito
+  if (rgFormatado.length > 12) {
+      rgFormatado = rgFormatado.substring(0, 12); // Limita o comprimento a 12 caracteres
+  }
+  return rgFormatado;
+}
+
+function formatarCPF(cpf) {
+  var cpfFormatado = cpf.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+  cpfFormatado = cpfFormatado.replace(/^(\d{3})(\d)/, '$1.$2'); // Insere um ponto após o terceiro dígito
+  cpfFormatado = cpfFormatado.replace(/^(\d{3})\.(\d{3})(\d)/, '$1.$2.$3'); // Insere um ponto após o sexto dígito
+  cpfFormatado = cpfFormatado.replace(/^(\d{3})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3-$4'); // Insere um hífen após o nono dígito
+  if (cpfFormatado.length > 14) {
+      cpfFormatado = cpfFormatado.substring(0, 14); // Limita o comprimento a 14 caracteres
+  }
+  return cpfFormatado;
+}
+
+function formatarTelefone(telefone) {
+  var telefoneFormatado = telefone.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+  telefoneFormatado = telefoneFormatado.replace(/^(\d{2})(\d)/, '($1) $2'); // Insere parênteses em torno dos dois primeiros dígitos
+  if (telefoneFormatado.length < 14) {
+      telefoneFormatado = telefoneFormatado.replace(/(\(\d{2}\) \d{4})(\d)/, '$1-$2'); // Insere um hífen após o sexto dígito para telefones com 8 dígitos
+  } else {
+      telefoneFormatado = telefoneFormatado.replace(/(\(\d{2}\) \d{5})(\d)/, '$1-$2'); // Insere um hífen após o sétimo dígito para telefones com 9 dígitos
+  }
+  // Limita o telefoneFormatado a 15 caracteres
+  if (telefoneFormatado.length > 15) {
+      telefoneFormatado = telefoneFormatado.substring(0, 15);
+  }
+  return telefoneFormatado;
+}
+
+function formatarData(data) {
+  var dataFormatada = data.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+  dataFormatada = dataFormatada.replace(/^(\d{2})(\d)/, '$1/$2'); // Insere uma barra após o segundo dígito
+  dataFormatada = dataFormatada.replace(/^(\d{2})\/(\d{2})(\d)/, '$1/$2/$3'); // Insere uma barra após o quarto dígito
+  if (dataFormatada.length > 10) {
+      dataFormatada = dataFormatada.substring(0, 10); // Limita o comprimento a 10 caracteres
+  }
+  return dataFormatada;
+}
+
+function calcularIdade(data_nascimento) {
+  var hoje = new Date();
+  var nascimento = new Date(data_nascimento);
+  var idade = hoje.getFullYear() - nascimento.getFullYear();
+  var m = hoje.getMonth() - nascimento.getMonth();
+
+  if (m < 0 || (m === 0 && hoje.getDate() < nascimento.getDate())) {
+    idade--;
+  }
+
+  var responsavel = document.getElementById('responsavel');
+  var cpfResponsavel = document.getElementById('cpf_responsavel');
+
+  if (idade < 18) {
+    responsavel.required = true;
+    cpfResponsavel.required = true;
+    alert('Paciente menor de idade. Informe o responsável e cpf.');
+  } else {
+    responsavel.required = false;
+    cpfResponsavel.required = false;
+  }
+}
+
+
+document.getElementById('cep').addEventListener('input', function (event) {
+  this.value = formatarCEP(this.value);
+});
+
+document.getElementById('rg').addEventListener('input', function (event) {
+  this.value = formatarRG(this.value);
+});
+
+document.getElementById('cpf').addEventListener('input', function (event) {
+  this.value = formatarCPF(this.value);
+});
+
+document.getElementById('telefone').addEventListener('input', function (event) {
+  this.value = formatarTelefone(this.value);
+});
+
+document.getElementById('data_nascimento').addEventListener('input', function (event) {
+  this.value = formatarData(this.value);
+});
+
+document.getElementById('data_nascimento').addEventListener('input', function() {
+  calcularIdade(this.value);
+});
